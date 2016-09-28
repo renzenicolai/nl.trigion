1d64c1c9b83d9e069bce729b486c2c225736d352
Emile Nijssen <emile@athom.nl>
2016-09-28 17:58:27 +0200

    fixes #1

--- app.js
+++ app.js
@@ -9,73 +9,83 @@ function init() {
 	Homey.manager('flow').on('action.schedule', function( callback, args ){
 
 		var user = Homey.manager('settings').get('user');
+		if( user ) {
 
-		var t = new Trigion({
-			username: user.username,
-			password: user.password
-		})
+			var t = new Trigion({
+				username: user.username,
+				password: user.password
+			})
 
-		var now = new Date();
-		args.time = args.time.split(':');
-		var alarmTime = new Date( now.getFullYear(), now.getMonth(), now.getDate(), args.time[0], args.time[1] );
+			var now = new Date();
+			args.time = args.time.split(':');
+			var alarmTime = new Date( now.getFullYear(), now.getMonth(), now.getDate(), args.time[0], args.time[1] );
 
-		// if time is less than now, next day
-		if( alarmTime < now ) {
-			alarmTime.setDate(alarmTime.getDate() + 1);
-		}
+			// if time is less than now, next day
+			if( alarmTime < now ) {
+				alarmTime.setDate(alarmTime.getDate() + 1);
+			}
 
-		t.login(function( err, result ){
-			if( err ) return callback(err.Error.ErrorText);
-
-			t.getObjects(function( err, result ){
+			t.login(function( err, result ){
 				if( err ) return callback(err.Error.ErrorText);
 
-				t.setAlarm({
-					reference	: result[0].AlarmObjects[0].R,
-					endTime		: alarmTime
-				}, function( err, result ){
+				t.getObjects(function( err, result ){
 					if( err ) return callback(err.Error.ErrorText);
-					if( result.ResultCode === 'Ok' ) {
-						return callback( null, true );
-					} else {
-						return callback( result.ResultText );
-					}
+
+					t.setAlarm({
+						reference	: result[0].AlarmObjects[0].R,
+						endTime		: alarmTime
+					}, function( err, result ){
+						if( err ) return callback(err.Error.ErrorText);
+						if( result.ResultCode === 'Ok' ) {
+							return callback( null, true );
+						} else {
+							return callback( result.ResultText );
+						}
+					})
 				})
 			})
-		})
+
+		} else {
+			callback( new Error( __('not_logged_in') ))
+		}
 
 	})
 
 	Homey.manager('flow').on('action.scheduleRelative', function( callback, args ){
 
 		var user = Homey.manager('settings').get('user');
+		if( user ) {
 
-		var t = new Trigion({
-			username: user.username,
-			password: user.password
-		})
+			var t = new Trigion({
+				username: user.username,
+				password: user.password
+			})
 
-		var endTime = new Date((new Date()).getTime() + (args.addHours*60*60*1000));
+			var endTime = new Date((new Date()).getTime() + (args.addHours*60*60*1000));
 
-		t.login(function( err, result ){
-			if( err ) return callback(err.Error.ErrorText);
-
-			t.getObjects(function( err, result ){
+			t.login(function( err, result ){
 				if( err ) return callback(err.Error.ErrorText);
 
-				t.setAlarm({
-					reference	: result[0].AlarmObjects[0].R,
-					endTime		: endTime
-				}, function( err, result ){
+				t.getObjects(function( err, result ){
 					if( err ) return callback(err.Error.ErrorText);
-					if( result.ResultCode === 'Ok' ) {
-						return callback( null, true );
-					} else {
-						return callback( result.ResultText );
-					}
+
+					t.setAlarm({
+						reference	: result[0].AlarmObjects[0].R,
+						endTime		: endTime
+					}, function( err, result ){
+						if( err ) return callback(err.Error.ErrorText);
+						if( result.ResultCode === 'Ok' ) {
+							return callback( null, true );
+						} else {
+							return callback( result.ResultText );
+						}
+					})
 				})
 			})
-		})
+
+		} else {
+			callback( new Error( __('not_logged_in') ))
+		}
 
 	})
 
